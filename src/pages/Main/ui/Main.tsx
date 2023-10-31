2706da129071e53b2e4fd028a63ec3549cf13673
a.bestuzhev <a.bestuzhev@notamedia.ru>
2023-10-31 20:44:41 +0300

    Корректировка jest конфига

--- src/pages/Main/ui/Main.tsx
+++ src/pages/Main/ui/Main.tsx
@@ -1,4 +1,9 @@
 const Main = () => {
+    const str = '20.10.2395 Ваша заявка с номером #482784 офррмлена'
+    const reg = /(\d{2}\.){2}(19\d{2}|2(0|1|2)\d{2}|\d{2})(?!\d)/
+
+    console.log('!!!', reg.exec(str))
+    console.log('----------------')
     return (
         <div>
             Main
